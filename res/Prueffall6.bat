cd5eb95d4012f6faa045c0637e83a6ad827c1175
Falko Schumann <falko.schumann@bitctrl.de>
2008-02-22 14:35:05 +0000

    JUnit aus den einzelnen SWE entfernt und in sys.funclib.bitctrl eingefügt.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.iav.gllib@6898 40c0be50-5d29-0410-901d-fb59edb5cafc

--- res/Prueffall6.bat
+++ res/Prueffall6.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET CP=de.bsvrz.iav.gllib-runtime.jar;de.bsvrz.iav.gllib-test.jar;lib\junit-4.1.jar
+SET CP=de.bsvrz.iav.gllib-runtime.jar;de.bsvrz.iav.gllib-test.jar
 
 java -cp %CP% org.junit.runner.JUnitCore de.bsvrz.iav.gllib.gllib.Prueffall6
 
