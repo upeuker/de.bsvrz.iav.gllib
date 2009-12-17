ee80307a35443c23be58fff25841b89431f731ab
Enrico Schnepel <info@bitctrl.de>
2009-12-17 09:52:11 +0000

    konvertieren für Ganglinien aus der Historie
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.iav.gllib@20827 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/iav/gllib/gllib/dav/GanglinieUtil.java
+++ src/de/bsvrz/iav/gllib/gllib/dav/GanglinieUtil.java
@@ -7,9 +7,11 @@ import java.util.List;
 import com.bitctrl.util.Interval;
 
 import de.bsvrz.sys.funclib.bitctrl.modell.att.Feld;
+import de.bsvrz.sys.funclib.bitctrl.modell.tmganglinienglobal.attribute.AtlGanglinie;
 import de.bsvrz.sys.funclib.bitctrl.modell.tmganglinienglobal.attribute.AtlPrognoseGanglinie;
 import de.bsvrz.sys.funclib.bitctrl.modell.tmganglinienglobal.attribute.AtlPrognoseGanglinienAnfrage;
 import de.bsvrz.sys.funclib.bitctrl.modell.tmganglinienglobal.attribute.AtlStuetzstelle;
+import de.bsvrz.sys.funclib.bitctrl.modell.tmverkehrglobal.objekte.MessQuerschnittAllgemein;
 
 public final class GanglinieUtil {
 
@@ -38,6 +40,33 @@ public final class GanglinieUtil {
 		return result;
 	}
 
+	public static List<GanglinieMQ> konvertiere(
+			final Feld<AtlGanglinie> ganglinien,
+			final MessQuerschnittAllgemein mq) {
+		final List<GanglinieMQ> result = new ArrayList<GanglinieMQ>();
+		for (final AtlGanglinie g : ganglinien) {
+			final GanglinieMQ ganglinie = new GanglinieMQ();
+			ganglinie.setMessQuerschnitt(mq);
+			ganglinie.setEreignisTyp(g.getEreignisTyp());
+			ganglinie.setAnzahlVerschmelzungen(g.getAnzahlVerschmelzungen()
+					.longValue());
+			ganglinie
+					.setApproximationDaK(g.getGanglinienVerfahren().getValue());
+			ganglinie.setBSplineOrdnung(g.getOrdnung().intValue());
+			ganglinie.setLetzteVerschmelzung(g.getLetzteVerschmelzung()
+					.getTime());
+			ganglinie.setReferenz(0 != g.getReferenzganglinie().byteValue());
+			for (final AtlStuetzstelle s : g.getStuetzstelle()) {
+				ganglinie.put(s.getZeit().getTime(), new Messwerte(s.getQKfz()
+						.getValue(), s.getQLkw().getValue(), s.getVPkw()
+						.getValue(), s.getVLkw().getValue()));
+			}
+
+			result.add(ganglinie);
+		}
+		return result;
+	}
+
 	public static List<AtlPrognoseGanglinienAnfrage> konvertiere(
 			final Collection<GlProgAnfrage> anfragen) {
 		final List<AtlPrognoseGanglinienAnfrage> result = new ArrayList<AtlPrognoseGanglinienAnfrage>();
@@ -48,5 +77,4 @@ public final class GanglinieUtil {
 	private GanglinieUtil() {
 		// utility class
 	}
-
 }
